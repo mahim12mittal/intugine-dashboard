c572bb63427fe61726896225f6c34173ebe5dd5a
Mahim Mittal <mahim12mittal@gmail.com>
2020-08-07 14:21:36 +0530

    Updates

---
+++ precache-manifest.0cf8a61e75f2812dcc59efab55bd07b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3acd509654c603dbb5e766e16ac716",
+    "url": "/intugine-dashboard.github.io/index.html"
+  },
+  {
+    "revision": "cf32b928333d811867a9",
+    "url": "/intugine-dashboard.github.io/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a0bf7d147283e65be22a",
+    "url": "/intugine-dashboard.github.io/static/css/main.784329b1.chunk.css"
+  },
+  {
+    "revision": "cf32b928333d811867a9",
+    "url": "/intugine-dashboard.github.io/static/js/2.33c3596a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intugine-dashboard.github.io/static/js/2.33c3596a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bf7d147283e65be22a",
+    "url": "/intugine-dashboard.github.io/static/js/main.59a7e9b7.chunk.js"
+  },
+  {
+    "revision": "f0d8606c10cc4b10a0b1",
+    "url": "/intugine-dashboard.github.io/static/js/runtime-main.5454f757.js"
+  },
+  {
+    "revision": "85f9fdcc34b168fd80fa48babd0e1b99",
+    "url": "/intugine-dashboard.github.io/static/media/destination.85f9fdcc.svg"
+  },
+  {
+    "revision": "877653aeb01976fd7edf924c661eacc2",
+    "url": "/intugine-dashboard.github.io/static/media/logo.877653ae.svg"
+  },
+  {
+    "revision": "52b703a8be7195bf860d5e89cdc1ec6e",
+    "url": "/intugine-dashboard.github.io/static/media/profile.52b703a8.svg"
+  },
+  {
+    "revision": "1f5bfd3e805919a486ffb8e1ab44785a",
+    "url": "/intugine-dashboard.github.io/static/media/warehouse.1f5bfd3e.svg"
+  }
+]);
